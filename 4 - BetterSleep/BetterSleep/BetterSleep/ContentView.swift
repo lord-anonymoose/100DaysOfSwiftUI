5fb058a5fd97edaf49f4792664bb013ef3221495
Philipp <severus99@icloud.com>
2021-01-02 01:02:10 +0300

    Worked on UI part of BetterSleep app

--- 4 - BetterSleep/BetterSleep/BetterSleep/ContentView.swift
+++ 4 - BetterSleep/BetterSleep/BetterSleep/ContentView.swift
@@ -10,19 +10,29 @@ import SwiftUI
 struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date()
+    @State private var coffeAmount = 1
     
     var body: some View {
-        
-        Stepper(value: $sleepAmount, in: 4...12) {
-            Text("\(sleepAmount, specifier: "%g") hours")
+        NavigationView {
+            //Form {
+            VStack(alignment: .leading) {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Select time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                    .padding(.bottom, 20)
+                Text("Desired amount of sleep:")
+                    .font(.headline)
+                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                    Text("\(sleepAmount, specifier: "%g") hours")
+                    
+                }
+                .padding(.bottom, 20)
+            }
+            .padding(.leading, 10)
+            .padding(.trailing, 10)
+            //}
         }
-        DatePicker(
-            "Enter date",
-            selection: $wakeUp,
-            in: Date()...,
-            displayedComponents: [.hourAndMinute]
-        )
-            .labelsHidden()
     }
 }
 
