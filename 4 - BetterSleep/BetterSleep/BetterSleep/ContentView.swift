d4247cba554ef83f108feab16afd9ceffa75aa0e
Philipp <severus99@icloud.com>
2021-01-02 15:15:47 +0300

    First working version of BetterSleep app

--- 4 - BetterSleep/BetterSleep/BetterSleep/ContentView.swift
+++ 4 - BetterSleep/BetterSleep/BetterSleep/ContentView.swift
@@ -7,14 +7,43 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date()
-    @State private var coffeAmount = 1
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    
+    func calculateBedTime () {
+        let model = SleepCalculator()
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        
+        do {
+            let prediction = try
+                model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let bedTime = wakeUp - prediction.actualSleep
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = formatter.string(from: bedTime)
+
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, something went wrong!"
+        }
+        showingAlert = true
+    }
     
     var body: some View {
         NavigationView {
-            //Form {
             VStack(alignment: .leading) {
                 Text("When do you want to wake up?")
                     .font(.headline)
@@ -28,10 +57,29 @@ struct ContentView: View {
                     
                 }
                 .padding(.bottom, 20)
+                
+                Text("Amount of coffee intake:")
+                    .font(.headline)
+                Stepper(value: $coffeeAmount, in: 1...20, step: 1) {
+                    if (coffeeAmount == 1)  {
+                        Text ("1 cup")
+                    } else {
+                        Text ("\(coffeeAmount) cups")
+                    }
+                }
             }
-            .padding(.leading, 10)
-            .padding(.trailing, 10)
-            //}
+            .navigationBarTitle("Better Sleep")
+            .navigationBarItems(trailing:
+                                    Button(action: calculateBedTime) {
+                                        Text("Calculate")
+                                    }
+            )
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text(alertTitle), message: Text(alertMessage)
+                      , dismissButton: .default(Text("OK")))
+            }
+            .padding(.leading, 20)
+            .padding(.trailing, 20)
         }
     }
 }
