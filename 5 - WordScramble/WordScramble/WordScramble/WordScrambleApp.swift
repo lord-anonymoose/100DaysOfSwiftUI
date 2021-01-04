979d963063002de8f8287f8fb95757021851a6cd
Philipp <severus99@icloud.com>
2021-01-04 13:09:50 +0300

    Added files for project 5.

---
+++ 5 - WordScramble/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Philipp on 04.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
