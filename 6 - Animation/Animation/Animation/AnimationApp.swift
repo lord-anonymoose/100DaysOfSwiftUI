52506088aba94d48a6305289e4533b99b70edf96
Philipp <severus99@icloud.com>
2021-01-07 14:45:19 +0300

    Added files for project 6

---
+++ 6 - Animation/Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Philipp on 07.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
