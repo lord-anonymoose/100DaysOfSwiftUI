b84edced73c5cb618253afd0b3cdf3a442c28746
Philipp <severus99@icloud.com>
2021-01-10 21:15:34 +0300

    Working on challenge task

---
+++ Edutainment/Edutainment/gameView.swift
@@ -0,0 +1,8 @@
+//
+//  gameView.swift
+//  Edutainment
+//
+//  Created by Philipp on 10.01.2021.
+//
+
+import Foundation
---
+++ Edutainment/Edutainment/settingsView.swift
@@ -0,0 +1,8 @@
+//
+//  settingsView.swift
+//  Edutainment
+//
+//  Created by Philipp on 10.01.2021.
+//
+
+import Foundation
