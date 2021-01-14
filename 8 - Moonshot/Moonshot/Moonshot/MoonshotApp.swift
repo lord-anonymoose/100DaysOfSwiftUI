70f739be6fbe55798308e7794f391496221220c9
Philipp <severus99@icloud.com>
2021-01-14 18:17:32 +0300

    Added empty files for project 8

---
+++ 8 - Moonshot/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Philipp on 14.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
