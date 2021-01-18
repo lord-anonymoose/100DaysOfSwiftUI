ad082fe6be598335b98d354d894f35fbf8e2ad5b
Philipp <severus99@icloud.com>
2021-01-18 14:06:00 +0300

    Started working on Day 43

---
+++ 9 - Drawing/Drawing/Drawing/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Philipp on 18.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Path { path in
+            path.move(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+        }
+        //.fill(Color.green)
+        .stroke(Color.blue, style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 9 - Drawing/Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Philipp on 18.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
