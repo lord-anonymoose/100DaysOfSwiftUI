5240bda61c29bb0d2b4235d4ceba92271c6b7027
Philipp <severus99@icloud.com>
2021-01-30 20:08:42 +0300

    Started working on Day 53: Project 11

---
+++ 11 - Bookworm/Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Philipp on 30.01.2021.
+//
+
+import SwiftUI
+import CoreData
+
+struct PushButton: View {
+    let title: String
+    @State var isOn: Bool
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
+    var body: some View {
+        Button(title) {
+            self.isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ?  0 : 5)
+    }
+}
+
+struct ContentView: View {
+    @State private var rememberMe = false
+    
+    var body: some View {
+        Toggle(isOn: $rememberMe) {
+            Text("Remember me")
+        }
+        PushButton(title: "Remember me", isOn: rememberMe)
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
